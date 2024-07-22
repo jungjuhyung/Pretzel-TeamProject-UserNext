3c8c410b4cd8956aec3a3706a771a9f020230f6a
wngud <ramyun1537@gmail.com>
2024-07-23 03:41:08 +0900

    영화 보는 페이지에 ai함수들 추가

--- app/park/detail/moviePlayPage/page.jsx
+++ app/park/detail/moviePlayPage/page.jsx
@@ -6,13 +6,42 @@ import '@/styles/commons/commons.css'
 
 import { Subtitle, Video } from "@/styles/park/moviePlayPageCSS";
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import LoadingSpinner from '@/app/commons/loadingSpinner/page';
 import { useStores } from '@/stores/StoreContext';
 
 const MoviePlayPage = () => {
-    const { loginStore, movieDetailStore, profileStore } = useStores();
+
+    // 비디오 태그 영상 경로에 직접 참조하여 접근하는 hook
+    const videoRef = useRef(null);
+    
+    // 재생 중인 시간 업데이트 상태변수
+    const [currentTime, setCurrentTime] = useState(0);
+
+    const { loginStore, movieDetailStore } = useStores();
+
+    // 감정분석 데이터 유무
+    const [emotionState, setEmotionState] = useState(true);
+
+    // 감정분석 데이터
+    const [emotionData, setEmotionData] = useState({})
+
+    // 딥페이스 데이터 유무
+    const [deepfaceState, setDeepfaceState] = useState(true);
+    
+    // 딥페이스 데이터
+    const [deepfaceData, setDeepfaceData] = useState({})
+    
+    // Storage에 존재하는 자막 배열
     const [canSubtitles, setCanSubtitles] = useState([]);
+
+    // 영화 재생 정보
+    const [moviePlay, setMoviePlay] = useState({});
+
+    // 로딩 상태
+    const [isLoading, setIsLoading] = useState(true);
+    
+    // 번역 코드 및 명칭
     const country = ["ko", "ja", "zh", "en", "fr", "de", "es", "it", "pt", "ru", "hi"];
     const labels = {
         ko: "한국어",
@@ -26,13 +55,31 @@ const MoviePlayPage = () => {
         pt: "Português",
         ru: "Русский",
         hi: "हिन्दी"
-      };
+    };
     
-    // 영화 재생 정보
-    const [moviePlay, setMoviePlay] = useState({});
+    // json으로 온 문자열 시간값을 정수형 초단위로 변환 함수
+    const changeSecondTime = (timeString) => {
+        const [hours, minutes, seconds] = timeString.split(':');
+        const [secs, millis] = seconds.split('.');
+         return (parseInt(hours, 10) * 3600)
+            + (parseInt(minutes, 10) * 60)
+            + parseInt(secs, 10)
+            + (parseInt(millis, 10) / 1000);
+    };
 
-    // 로딩 상태
-    const [isLoading, setIsLoading] = useState(true);
+    // 비디오의 특정 시간대로 이동하는 함수
+    const moveVideoTime = (videoTime) => {
+        if (videoRef.current) {
+            videoRef.current.currentTime = videoTime;
+        }
+    };
+
+    // 현재 재생 시간 업데이트 함수
+    const updateCurrentTime = () => {
+        if (videoRef.current) {
+            setCurrentTime(videoRef.current.currentTime);
+        }
+    };
 
     // 처음 렌더링 될 때 실행
     useEffect(() => {
@@ -78,8 +125,40 @@ const MoviePlayPage = () => {
                     profile_idx: loginStore.profile_idx
                 }
             });
-            console.log("tsts",response.data.movie_url);
+
+            // 스토리지 번역 파일 유무 체크
             checkSubtitles(response.data.movie_url)
+
+            // 감정분석 데이터가 없으면
+            if (response.data.ai_emotion_syc === null) {
+                setEmotionState(false)
+            } else {
+                try {
+                    const response4 = await axios.get(
+                        `/storage/pretzel-emotionResAI/${response.data.storage_name}_emotion_res.json`);
+                    if (response4.data) {
+                        console.log(response4.data)
+                        setEmotionData(response4.data)
+                    }
+                } catch (error) {
+                    console.error('감정분석 가져오기 실패 : ', error);
+                }
+            }
+            // 딥페이스 데이터가 없으면
+            if (response.data.ai_deep_syc === null) {
+                setDeepfaceState(false)
+            } else {
+                try {
+                    const response5 = await axios.get(
+                        `/storage/pretzel-deepfaceAI/${response.data.storage_name}_deepface.json`);
+                    if (response5.data) {
+                        console.log(response5.data)
+                        setDeepfaceData(response5.data)
+                    }
+                } catch (error) {
+                    console.error('딥페이스 가져오기 실패 : ', error);
+                }
+            }
         } catch (error) {
             console.error('상세 정보 가져오기 실패 : ', error);
         } finally {
@@ -92,6 +171,7 @@ const MoviePlayPage = () => {
         return <LoadingSpinner />
     }
 
+    // 스토리지에 번역 파일이 있는지 체크하는 함수
     async function checkSubtitles(name){
         const chk = [];
         for (const k of country) {
@@ -109,12 +189,14 @@ const MoviePlayPage = () => {
     return (
         <>
             <Video
-                src={`/storage/${moviePlay.movie_url}.mp4`}
+                ref={videoRef}
+                onTimeUpdate={updateCurrentTime}
                 controls
                 autoPlay
                 muted
                 controlsList="nodownload"
                 >
+                <source src={`/storage/${moviePlay.movie_url}.mp4`} />
                 {canSubtitles.map((k) => (
                     <Subtitle
                       key={k}
@@ -127,6 +209,8 @@ const MoviePlayPage = () => {
                 ))
                 }
             </Video>
+            <button onClick={() => moveVideoTime("30")}>Go to 30 seconds</button>
+            <div>현재 재생 시간: {currentTime}초</div>
         </>
     )
 }
