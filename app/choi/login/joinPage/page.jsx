2367234956b65dec3b338a158acc20c42c839c5a
choi <minchoi@naver.com>
2024-07-27 19:46:25 +0900

    0727-chm-console.log 없애기

--- app/choi/login/joinPage/page.jsx
+++ app/choi/login/joinPage/page.jsx
@@ -1,20 +1,44 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
-import { Global } from '@emotion/react';
-import DatePicker from 'react-datepicker';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Global } from "@emotion/react";
+import DatePicker from "react-datepicker";
+import axios from "axios";
 import { useRouter } from "next/navigation";
 
 import {
-  globalStyles, Background, JoinBox, Title, Name,
-  IdContainer, IdInput, IdCondition, PassWord, PwCondition, Re_PassWord,
-  Error, Email_Box, Email, Icon, Email_select,
-  BirthDay, NoticeAllBox, CheckBoxAll, NoticeBox_first, NoticeBox,
-  Notice_Title_Main, CheckBox_Basic, Notice_Title_Basic, Notice_Icon,
-  CheckBox_Choice, Notice_Title_Choice, JoinButton, GreenText , Wrapper_All,
-  IdCheckButton
-} from '../../../../styles/choi/login/JoinPageCSS';
+  globalStyles,
+  Background,
+  JoinBox,
+  Title,
+  Name,
+  IdContainer,
+  IdInput,
+  IdCondition,
+  PassWord,
+  PwCondition,
+  Re_PassWord,
+  Error,
+  Email_Box,
+  Email,
+  Icon,
+  Email_select,
+  BirthDay,
+  NoticeAllBox,
+  CheckBoxAll,
+  NoticeBox_first,
+  NoticeBox,
+  Notice_Title_Main,
+  CheckBox_Basic,
+  Notice_Title_Basic,
+  Notice_Icon,
+  CheckBox_Choice,
+  Notice_Title_Choice,
+  JoinButton,
+  GreenText,
+  Wrapper_All,
+  IdCheckButton,
+} from "../../../../styles/choi/login/JoinPageCSS";
 
 const JoinPage = () => {
   const [allChecked, setAllChecked] = useState(false);
@@ -29,27 +53,27 @@ const JoinPage = () => {
   });
 
   const [uvo, setUvo] = useState({
-    name: '',
-    user_id: '',
-    pwd: '',
-    repwd: '',
-    email: '',
+    name: "",
+    user_id: "",
+    pwd: "",
+    repwd: "",
+    email: "",
   });
 
   function handleInputChange(e) {
     const { name, value } = e.target;
 
-    if (name === 'name') {
+    if (name === "name") {
       const koreanOnly = /^[^a-zA-Z]*$/;
       if (!koreanOnly.test(value)) {
-        alert('이름은 한글만 입력 가능합니다.');
+        alert("이름은 한글만 입력 가능합니다.");
         return;
       }
     }
 
     setUvo({
       ...uvo,
-      [name]: value
+      [name]: value,
     });
   }
 
@@ -68,21 +92,21 @@ const JoinPage = () => {
   };
 
   const handleRequiredCheckChange = (name) => {
-    setRequiredChecks(prevState => ({
+    setRequiredChecks((prevState) => ({
       ...prevState,
       [name]: !prevState[name],
     }));
   };
 
   const handleOptionalCheckChange = (name) => {
-    setOptionalChecks(prevState => ({
+    setOptionalChecks((prevState) => ({
       ...prevState,
       [name]: !prevState[name],
     }));
   };
 
-  const [emailId, setEmailId] = useState('');
-  const [domain, setDomain] = useState('');
+  const [emailId, setEmailId] = useState("");
+  const [domain, setDomain] = useState("");
 
   const handleEmailId = (e) => {
     setEmailId(e.target.value);
@@ -102,47 +126,50 @@ const JoinPage = () => {
   async function handleIdCheck() {
     const idPattern = /^[a-z0-9]{6,12}$/;
     if (!idPattern.test(uvo.user_id)) {
-      alert('아이디는 영문 소문자와 숫자의 조합으로 6~12자리여야 합니다.');
+      alert("아이디는 영문 소문자와 숫자의 조합으로 6~12자리여야 합니다.");
       return;
     } else {
-      console.log('보내는 데이터 : ', uvo);
       try {
-        const response = await axios.post('/user/id_check', { user_id: uvo.user_id });
+        const response = await axios.post("/user/id_check", {
+          user_id: uvo.user_id,
+        });
         if (response.data === 1) {
           alert("중복아이디 있음");
         } else {
           alert("사용가능");
         }
       } catch (error) {
-        console.error('실패 : ', error);
+        console.error("실패 : ", error);
       }
     }
   }
 
   const handleEmailCheck = async () => {
-    if (!emailId || domain === '이메일 선택') {
+    if (!emailId || domain === "이메일 선택") {
       alert("이메일 주소를 선택해주세요.");
       return;
     }
 
     const fullEmail = `${emailId}@${domain}`;
-    console.log('보내는 데이터 : ', { email: fullEmail });
 
     try {
-      const response = await axios.post('/user/email_check', { email: fullEmail });
-      console.log('결과 : ', response.data);
+      const response = await axios.post("/user/email_check", {
+        email: fullEmail,
+      });
+
       if (response.data === 1) {
         alert("중복이메일 있음");
       } else {
         alert("사용가능");
       }
     } catch (error) {
-      console.error('실패 : ', error);
+      console.error("실패 : ", error);
     }
   };
 
   const validatePassword = (password) => {
-    const passwordPattern = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[~!@#$%^&*])[a-zA-Z\d~!@#$%^&*]{8,15}$/;
+    const passwordPattern =
+      /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[~!@#$%^&*])[a-zA-Z\d~!@#$%^&*]{8,15}$/;
     return passwordPattern.test(password);
   };
 
@@ -158,9 +185,11 @@ const JoinPage = () => {
     } else if (pwd !== repwd) {
       alert("비밀번호가 일치하지 않습니다.");
     } else if (!validatePassword(pwd)) {
-      alert('비밀번호는 영문, 숫자, 특수문자(~!@#$%^&*)를 포함하여 8~15자리여야 합니다.');
+      alert(
+        "비밀번호는 영문, 숫자, 특수문자(~!@#$%^&*)를 포함하여 8~15자리여야 합니다."
+      );
     } else {
-      const API_URL = '/user/join';
+      const API_URL = "/user/join";
       try {
         const response = await axios.post(API_URL, uvo);
         if (response.data === 1) {
@@ -168,7 +197,7 @@ const JoinPage = () => {
           router.push("/choi/login/loginPage");
         }
       } catch (error) {
-        console.log('error');
+        console.log("error");
       }
     }
   };
@@ -180,26 +209,53 @@ const JoinPage = () => {
         <JoinBox>
           <Wrapper_All>
             <Title>회원가입</Title>
-            <Name type='text' placeholder='이름' name='name' onChange={handleInputChange} />
+            <Name
+              type="text"
+              placeholder="이름"
+              name="name"
+              onChange={handleInputChange}
+            />
             <IdContainer>
-              <IdInput type='text' placeholder='아이디' name='user_id' onChange={handleInputChange} />
-              <IdCheckButton type='button' onClick={handleIdCheck}>중복확인</IdCheckButton>
+              <IdInput
+                type="text"
+                placeholder="아이디"
+                name="user_id"
+                onChange={handleInputChange}
+              />
+              <IdCheckButton type="button" onClick={handleIdCheck}>
+                중복확인
+              </IdCheckButton>
             </IdContainer>
             <IdCondition>영문 소문자 ,숫자 조합 6~12자리</IdCondition>
-            <PassWord type='password' placeholder='비밀번호' name='pwd' onChange={handleInputChange} />
-            <PwCondition>영문 , 숫자 , 특수문자(~!@#$%^&*)조합 8~15자리 </PwCondition>
-            <Re_PassWord type='password' placeholder='비밀번호 재입력' name='repwd' onChange={handleInputChange} />
+            <PassWord
+              type="password"
+              placeholder="비밀번호"
+              name="pwd"
+              onChange={handleInputChange}
+            />
+            <PwCondition>
+              영문 , 숫자 , 특수문자(~!@#$%^&*)조합 8~15자리{" "}
+            </PwCondition>
+            <Re_PassWord
+              type="password"
+              placeholder="비밀번호 재입력"
+              name="repwd"
+              onChange={handleInputChange}
+            />
             <Error>
-              {uvo.pwd !== uvo.repwd && uvo.repwd && (
-                <>비밀번호 불일치!</>
-              )}
+              {uvo.pwd !== uvo.repwd && uvo.repwd && <>비밀번호 불일치!</>}
               {uvo.pwd === uvo.repwd && uvo.repwd && (
                 <GreenText>비밀번호 일치!</GreenText>
               )}
             </Error>
             <IdContainer>
               <Email_Box>
-                <Email type='text' placeholder='이메일' name='email' onChange={handleEmailId} />
+                <Email
+                  type="text"
+                  placeholder="이메일"
+                  name="email"
+                  onChange={handleEmailId}
+                />
                 <Icon>@</Icon>
                 <Email_select onChange={handleDomain} defaultValue="">
                   <option value="">이메일 선택</option>
@@ -208,51 +264,93 @@ const JoinPage = () => {
                   <option value="gmail.com">gmail.com</option>
                   <option value="nate.com">nate.com</option>
                 </Email_select>
-                <IdCheckButton type='button' onClick={handleEmailCheck}>중복확인</IdCheckButton>
+                <IdCheckButton type="button" onClick={handleEmailCheck}>
+                  중복확인
+                </IdCheckButton>
               </Email_Box>
             </IdContainer>
             <NoticeAllBox>
               <NoticeBox_first>
-                <CheckBoxAll type='checkbox' checked={allChecked} onChange={handleAllCheck} />
-                <Notice_Title_Main>필수 및 선택 항목을 모두 포함하여 동의합니다.</Notice_Title_Main>
+                <CheckBoxAll
+                  type="checkbox"
+                  checked={allChecked}
+                  onChange={handleAllCheck}
+                />
+                <Notice_Title_Main>
+                  필수 및 선택 항목을 모두 포함하여 동의합니다.
+                </Notice_Title_Main>
               </NoticeBox_first>
 
               <NoticeBox>
-                <CheckBox_Basic type='checkbox' checked={requiredChecks.age} onChange={() => handleRequiredCheckChange('age')} />
-                <Notice_Title_Basic>[필수] 만 14세 이상입니다.</Notice_Title_Basic>
-                <Notice_Icon src='/images/icons/ArrowDown.png' />
+                <CheckBox_Basic
+                  type="checkbox"
+                  checked={requiredChecks.age}
+                  onChange={() => handleRequiredCheckChange("age")}
+                />
+                <Notice_Title_Basic>
+                  [필수] 만 14세 이상입니다.
+                </Notice_Title_Basic>
+                <Notice_Icon src="/images/icons/ArrowDown.png" />
               </NoticeBox>
 
               <NoticeBox>
-                <CheckBox_Basic type='checkbox' checked={requiredChecks.terms} onChange={() => handleRequiredCheckChange('terms')} />
-                <Notice_Title_Basic>[필수] 서비스 이용약관 동의</Notice_Title_Basic>
-                <Notice_Icon src='/images/icons/ArrowDown.png' />
+                <CheckBox_Basic
+                  type="checkbox"
+                  checked={requiredChecks.terms}
+                  onChange={() => handleRequiredCheckChange("terms")}
+                />
+                <Notice_Title_Basic>
+                  [필수] 서비스 이용약관 동의
+                </Notice_Title_Basic>
+                <Notice_Icon src="/images/icons/ArrowDown.png" />
               </NoticeBox>
 
               <NoticeBox>
-                <CheckBox_Basic type='checkbox' checked={requiredChecks.privacy} onChange={() => handleRequiredCheckChange('privacy')} />
-                <Notice_Title_Basic>[필수] 개인정보 수집 및 이용 동의</Notice_Title_Basic>
-                <Notice_Icon src='/images/icons/ArrowDown.png' />
+                <CheckBox_Basic
+                  type="checkbox"
+                  checked={requiredChecks.privacy}
+                  onChange={() => handleRequiredCheckChange("privacy")}
+                />
+                <Notice_Title_Basic>
+                  [필수] 개인정보 수집 및 이용 동의
+                </Notice_Title_Basic>
+                <Notice_Icon src="/images/icons/ArrowDown.png" />
               </NoticeBox>
 
               <NoticeBox>
-                <CheckBox_Choice type='checkbox' checked={optionalChecks.privacyOptional} onChange={() => handleOptionalCheckChange('privacyOptional')} />
-                <Notice_Title_Choice>[선택] 개인정보 수집 및 이용 동의</Notice_Title_Choice>
-                <Notice_Icon src='/images/icons/ArrowDown.png' />
+                <CheckBox_Choice
+                  type="checkbox"
+                  checked={optionalChecks.privacyOptional}
+                  onChange={() => handleOptionalCheckChange("privacyOptional")}
+                />
+                <Notice_Title_Choice>
+                  [선택] 개인정보 수집 및 이용 동의
+                </Notice_Title_Choice>
+                <Notice_Icon src="/images/icons/ArrowDown.png" />
               </NoticeBox>
 
               <NoticeBox>
-                <CheckBox_Choice type='checkbox' checked={optionalChecks.marketing} onChange={() => handleOptionalCheckChange('marketing')} />
-                <Notice_Title_Choice>[선택] 마케팅 정보 수신 동의 </Notice_Title_Choice>
-                <Notice_Icon src='/images/icons/ArrowDown.png' />
+                <CheckBox_Choice
+                  type="checkbox"
+                  checked={optionalChecks.marketing}
+                  onChange={() => handleOptionalCheckChange("marketing")}
+                />
+                <Notice_Title_Choice>
+                  [선택] 마케팅 정보 수신 동의{" "}
+                </Notice_Title_Choice>
+                <Notice_Icon src="/images/icons/ArrowDown.png" />
               </NoticeBox>
             </NoticeAllBox>
-            <JoinButton type='button' value={'회원가입'} onClick={handleSubmit} />
+            <JoinButton
+              type="button"
+              value={"회원가입"}
+              onClick={handleSubmit}
+            />
           </Wrapper_All>
         </JoinBox>
       </Background>
     </>
   );
-}
+};
 
 export default JoinPage;
--- app/main/newContent/page.jsx
+++ app/main/newContent/page.jsx
@@ -1,65 +1,78 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { observer } from 'mobx-react-lite';
-import LoadingSpinner from '../../commons/loadingSpinner/page';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { observer } from "mobx-react-lite";
+import LoadingSpinner from "../../commons/loadingSpinner/page";
 
 import {
-    New_Contents, Contents_Title, Contents_Box, PosterWrapper, Poster, Movie_Title
-} from '@/styles/choi/main/mainNewCotentCSS';
+  New_Contents,
+  Contents_Title,
+  Contents_Box,
+  PosterWrapper,
+  Poster,
+  Movie_Title,
+} from "@/styles/choi/main/mainNewCotentCSS";
 
 const NewContent = observer(() => {
-    const [recent_list, setRecent_list] = useState([]);
-    const [isLoading, setIsLoading] = useState(true); // isLoading 상태 추가
-    const postersPerPage = 5; // 페이지당 포스터 수
+  const [recent_list, setRecent_list] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // isLoading 상태 추가
+  const postersPerPage = 5; // 페이지당 포스터 수
 
-    const API_URL = "/main/";
+  const API_URL = "/main/";
 
-    useEffect(() => {
-        chart_data();
-    }, []);
+  useEffect(() => {
+    chart_data();
+  }, []);
 
-    // 새로 올라온 콘텐츠 리스트 가져오기
-    async function chart_data() {
-        setIsLoading(true); // 데이터를 로드하기 전에 로딩 상태로 설정
-        try {
-            const response = await axios.post(API_URL + "recent_list");
-            if (response.data) {
-                setRecent_list(response.data);
-            }
-        } catch (error) {
-            console.error('상세 정보 가져오기 실패 : ', error);
-        } finally {
-            setIsLoading(false); // 데이터를 로드한 후 로딩 상태 해제
-        }
+  // 새로 올라온 콘텐츠 리스트 가져오기
+  async function chart_data() {
+    setIsLoading(true); // 데이터를 로드하기 전에 로딩 상태로 설정
+    try {
+      const response = await axios.post(API_URL + "recent_list");
+      if (response.data) {
+        setRecent_list(response.data);
+      }
+    } catch (error) {
+    } finally {
+      setIsLoading(false); // 데이터를 로드한 후 로딩 상태 해제
     }
+  }
 
-    // 로딩
-    if (isLoading) {
-        return <LoadingSpinner />;
-    }
+  // 로딩
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
 
-    const handlePosterClick = (movie_idx) => {
-        console.log(movie_idx);
-        // Your handlePosterClick function logic here
-    };
+  const handlePosterClick = (movie_idx) => {
+    // Your handlePosterClick function logic here
+  };
 
-    return (
-        <New_Contents>
-            <Contents_Title>새로 올라온 콘텐츠</Contents_Title>
-            <Contents_Box>
-                {recent_list.slice(0, postersPerPage).map((k) => (  // 첫 5개의 포스터만 렌더링
-                    <PosterWrapper key={k.movie_idx} onClick={() => handlePosterClick(k.movie_idx)}>
-                        <Poster src={`https://image.tmdb.org/t/p/w500${k.poster_url}`} className="poster-image" />
-                        <Movie_Title className="poster-title">
-                            {k.korea_title}
-                        </Movie_Title>
-                    </PosterWrapper>
-                ))}
-            </Contents_Box>
-        </New_Contents>
-    );
+  return (
+    <New_Contents>
+      <Contents_Title>새로 올라온 콘텐츠</Contents_Title>
+      <Contents_Box>
+        {recent_list.slice(0, postersPerPage).map(
+          (
+            k // 첫 5개의 포스터만 렌더링
+          ) => (
+            <PosterWrapper
+              key={k.movie_idx}
+              onClick={() => handlePosterClick(k.movie_idx)}
+            >
+              <Poster
+                src={`https://image.tmdb.org/t/p/w500${k.poster_url}`}
+                className="poster-image"
+              />
+              <Movie_Title className="poster-title">
+                {k.korea_title}
+              </Movie_Title>
+            </PosterWrapper>
+          )
+        )}
+      </Contents_Box>
+    </New_Contents>
+  );
 });
 
 export default NewContent;
